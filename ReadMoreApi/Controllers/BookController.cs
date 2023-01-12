7fe869684e4edce995ed963ac5dfff96680f78e0
Petru <pconstantin@launchcg.com>
2023-01-11 16:55:32 -0800

    Route Scoped

--- ReadMoreApi/Controllers/BookController.cs
+++ ReadMoreApi/Controllers/BookController.cs
@@ -8,12 +8,34 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-namespace ReadMoreApi
+namespace READMOREAPI
 {
     public class BookController
     {
         [FunctionName("BookGet")]
         public  async Task<IActionResult> BookGet(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "book/{bookid}")] HttpRequest req, int bookid,
+            ILogger log)
+        {
+            log.LogInformation("Creating New Book Item");
+
+            var result = "Testing";
+            return new OkObjectResult(await Task.FromResult(result));
+        }
+
+        [FunctionName("BookCreate")]
+        public  async Task<IActionResult> BookCreate(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "book")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("Creating New Book Item");
+
+            var result = "Testing";
+            return new OkObjectResult(await Task.FromResult(result));
+        }
+
+        [FunctionName("BookGetList")]
+        public  async Task<IActionResult> BookGetList(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "book")] HttpRequest req,
             ILogger log)
         {
@@ -22,5 +44,28 @@ namespace ReadMoreApi
             var result = "Testing";
             return new OkObjectResult(await Task.FromResult(result));
         }
+
+        [FunctionName("BookDelete")]
+        public  async Task<IActionResult> BookDelete(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "book")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("Creating New Book Item");
+
+            var result = "Testing";
+            return new OkObjectResult(await Task.FromResult(result));
+        }
+
+        [FunctionName("BookUpdate")]
+        public  async Task<IActionResult> BookUpdate(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "book")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("Creating New Book Item");
+
+            var result = "Testing";
+            return new OkObjectResult(await Task.FromResult(result));
+        }
+        
     }
 }
