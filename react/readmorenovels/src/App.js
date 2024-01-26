b38a728959bd2edb4e717d543d351f070de09778
Wyatt Haak (Launch Consulting) <v-wyatthaak@microsoft.com>
2024-01-26 10:42:59 -0800

    created the base project

---
+++ react/readmorenovels/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Read More Novels</h1>
+      <h2>My name is {process.env.REACT_APP_SAMPLE_VAR}</h2>
+    </div>
+  );
+}
+
+export default App;
