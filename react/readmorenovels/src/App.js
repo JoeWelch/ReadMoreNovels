17e89c62b0592d2714b557e6e5b7bd8a4a956684
Matt Burns <mburns@launchcg.com>
2024-02-10 01:33:47 -0800

    rough search layout

--- react/readmorenovels/src/App.js
+++ react/readmorenovels/src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+import SearchPage from './Components/Pages/SearchPage';
+import { Header } from 'semantic-ui-react';
 
 function App() {
   return (
     <div className="App">
-      <h1>Read More Novels</h1>
-      <h2>{process.env.REACT_APP_SAMPLE_VAR}</h2>
+      <Header as='h1' content="Read More Novels" textAlign='center' />
+      {/* <h2>{process.env.REACT_APP_SAMPLE_VAR}</h2> */}
+      <SearchPage />
     </div>
   );
 }
